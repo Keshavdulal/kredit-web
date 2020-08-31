b18ca2aa05d780ea65bb7a559130b2304d1aa6ca
keshavdulal <keshav.dulal@gmail.com>
2020-08-31 13:19:34 +0545

    nextjs + chakra-ui setup

---
+++ README.md
@@ -0,0 +1,11 @@
+```
+yarn create next-app --example with-chakra-ui with-chakra-ui-app
+```
+
+```
+yarn add --dev typescript @types/node
+```
+
+```
+yarn add urql garaphql
+```
---
+++ src/components/InputField.tsx
@@ -0,0 +1,37 @@
+import React, { InputHTMLAttributes } from 'react';
+import {
+    FormControl,
+    FormLabel,
+    Input,
+    FormErrorMessage,
+} from '@chakra-ui/core';
+import { useField } from 'formik';
+
+// fancy way of telling my Input field to take props
+// as any regular input element would take
+type InputFieldProps = InputHTMLAttributes<HTMLInputElement> & {
+    name: string;
+    label: string;
+};
+
+const InputField: React.FC<InputFieldProps> = ({
+    label,
+    size: _,
+    ...props
+}) => {
+    const [field, { error }] = useField(props);
+    return (
+        <FormControl isInvalid={!!error}>
+            <FormLabel htmlFor={field.name}>{label}</FormLabel>
+            <Input
+                {...field}
+                {...props}
+                id={field.name}
+                placeholder={props.placeholder}
+            />
+            {error && <FormErrorMessage>{error}</FormErrorMessage>}
+        </FormControl>
+    );
+};
+
+export default InputField;
